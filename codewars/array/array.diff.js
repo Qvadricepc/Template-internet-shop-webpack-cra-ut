2d8ced796c5ab427993aa33050b55950d4cb6ef8
Roman Sirmanov <roman.sirmanov@intellectsoft.net>
2021-12-01 10:31:49 +0200

    codewars_task_array_1: Array.diff

---
+++ codewars/array/array.diff.js
@@ -0,0 +1,15 @@
+function arrayDiff(a, b) {
+    let result = []
+    for (let i=0; i<a.length; ++i) {
+        if (b.indexOf(a[i]) == -1) {
+            result.push(a[i]);
+        }
+    }
+    return result;
+}
+
+
+console.log(arrayDiff([], [4,5]));
+console.log(arrayDiff([3,4], [3]));
+console.log(arrayDiff([1,8,2], []));
+console.log(arrayDiff([1,2,3], [1,2]));
