9bd9c2fdd0b992b1602802efc7e307d8ccfc5364
Roman Sirmanov <roman.sirmanov@intellectsoft.net>
2022-01-13 20:10:32 +0200

    feat: toast and pagination

--- src/products/components/products.tsx
+++ src/products/components/products.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useGetProductsQuery } from '../products-api-slice';
 import { TProduct } from '../types';
 import { Grid } from '@mui/material';
@@ -8,10 +8,18 @@ import { useAppSelector } from '../../app/hooks';
 import { selectCategory } from '../../layout/drawer-slice';
 import { selectSearch } from '../../layout/search-slice';
 
+import Stack from '@mui/material/Stack';
+import Pagination from '@mui/material/Pagination';
+
 export const Products: React.FC = () => {
   const pickedCategory = useAppSelector(selectCategory);
+  const [page, setPage] = useState(1);
   const search = useAppSelector(selectSearch);
-  const { data, isLoading, isError } = useGetProductsQuery({});
+  const { data, isLoading, isError } = useGetProductsQuery(page);
+
+  const handleChange = (e: any, p: number) => {
+    setPage(p);
+  };
 
   if (isLoading) {
     return <Loader />;
@@ -24,7 +32,7 @@ export const Products: React.FC = () => {
   }
 
   return (
-    <Grid container spacing={3} bgcolor="#e8eaf6">
+    <Grid container spacing={3} bgcolor="#e8eaf6" display="flex" justifyContent="center">
       {data
         .filter((cat: TProduct) =>
           pickedCategory === 'All products' ? true : cat.category === pickedCategory.toLowerCase()
@@ -33,6 +41,11 @@ export const Products: React.FC = () => {
         .map((product: TProduct) => {
           return <ProductCard product={product} key={product.id} />;
         })}
+      <Grid item display="flex" justifyContent="center">
+        <Stack spacing={2}>
+          <Pagination count={8} color="primary" onChange={handleChange} />
+        </Stack>
+      </Grid>
     </Grid>
   );
 };
--- src/products/products-api-slice.tsx
+++ src/products/products-api-slice.tsx
@@ -1,11 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { TProduct } from './types';
 
 export const productsApiSlice = createApi({
   reducerPath: 'products',
   baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  tagTypes: ['products'],
   endpoints: (builder) => ({
-    getProducts: builder.query({
-      query: () => '/products',
+    getProducts: builder.query<TProduct[], number>({
+      query: (page) => `products?_page=${page}&_limit=12`,
     }),
   }),
 });
